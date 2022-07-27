7ce631e442485400c0b2620fa57bc6daf201fe5c
ericeasthope <eric.easthope@me.com>
2022-07-27 16:28:47 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# hct-lab-site
---
+++ src/components/Application/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import "./Application.scss";
+
+const Application = () => {
+  return (
+    <div className="Application">
+      <div>Hello, World!</div>
+    </div>
+  );
+};
+
+export default Application;
